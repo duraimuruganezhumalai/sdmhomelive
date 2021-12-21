53cf5912fd3e5dd5decab91e3240dc62f3db378f
duraimurugane <durai.12murugan@gmail.com>
2021-12-21 18:12:09 +0530

    counter animation installed jquery, counterup and waypoints dependencies to output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { CommonModule } from '@angular/common';
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare let $:any;
+
 
 @Component({
   selector: 'app-home',
@@ -10,14 +12,28 @@ export class HomeComponent implements OnInit {
 
   
 
+
+
+  
+
   constructor() { 
-
     
+  }
+  ngOnInit() {
+    $('.counter').counterUp();
 
+  //   $('.counter-count').each( () => {
+  //     $(this).prop('Counter',0).animate({
+  //         Counter: $(this).text()
+  //     }, {
        
+  //         duration: 4000,
+  //         easing: 'swing',
+  //         step: function (now) {
+  //             $(this).text(Math.ceil(now));
+  //         }
+  //     });
+  // });
+    
   }
-
-  ngOnInit(): void {
-  }
-
 }
