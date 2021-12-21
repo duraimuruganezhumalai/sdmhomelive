237e24dfa0b38018fe35df851dd189bd632ac925
duraimurugane <durai.12murugan@gmail.com>
2021-12-21 18:50:04 +0530

    about us page our skill progress bar  80% completed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let $:any;
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,31 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
+    var lang = {
+      "html": "100%",
+      "css": "90%",
+      "javascript": "70%",
+      "php": "55%",
+      "angular": "65%"
+    };
+    
+    var multiply = 4;
+    
+    $.each( lang, function( language: string, pourcent: any) {
+    
+      var delay = 700;
+      
+      setTimeout(function() {
+        $('#'+language+'-pourcent').html(pourcent);
+      },delay*multiply);
+      
+      multiply++;
+    
+    });
+
+
   }
 
 }
