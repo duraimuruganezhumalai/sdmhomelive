c5ea8d0c0425ebda3e887849efeb3574d11748d0
duraimurugane <durai.12murugan@gmail.com>
2021-12-23 14:55:32 +0530

    album page image gallery popup updated

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let $: any;
 
 @Component({
   selector: 'app-album',
@@ -6,19 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.scss']
 })
 export class AlbumComponent implements OnInit {
- 
- 
 
 
-  
+
+
   constructor() {
 
 
-   }
+  }
 
-  ngOnInit(): void {
-    
-    
+  ngOnInit() {
+
+
+
+    $(".fancybox").fancybox({
+      openEffect: "none",
+      closeEffect: "none"
+    });
+
+    $(".zoom").hover(() => {
+
+      $(this).addClass('transition');
+    }, () => {
+
+      $(this).removeClass('transition');
+    });
+
+
+
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import GLightbox from 'glightbox';
+
 
 @Component({
   selector: 'app-root',
