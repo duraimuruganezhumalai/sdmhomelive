d4b261cfe9e8ecbde2531e0440ad133c28d0e5a4
duraimurugane <durai.12murugan@gmail.com>
2021-12-24 18:42:43 +0530

    portfolio page image comparision slider is updated

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,22 +8,49 @@ declare let $:any;
 })
 export class PortfolioComponent implements OnInit {
 
- 
+
 
   constructor() {
-
-   
+    
     
 
   }
 
   ngOnInit():void {
   
-       $("#horizontal").twentytwenty();
-      $("#horizontal").twentytwenty({
-      orientation: 'horizontal'
-      });
+    //  $("#horizontal").twentytwenty();
+    //   $("#horizontal").twentytwenty({
+    //   orientation: 'horizontal'
+    //   });
+$(function() {
+  $(".box[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+});
+//
+// // // OR
+// //
+// // $(function() {
+// //   $(".box").twentytwenty();
+// // });
+// To understand "{default_offset_pct:0.3}":
+// $(function() {
+//   $("#img1[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.3});
+// });
 
+// $(function() {
+//   $("#img2[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+// });
 
+// $(function() {
+//   $("#img3[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.8});
+// });
+
+// $(function() {
+//   $("#img4[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+// });
+
+// $(function() {
+//   $("#img5[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+// });
+      
   }
 }
