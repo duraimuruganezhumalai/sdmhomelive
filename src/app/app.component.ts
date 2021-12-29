4298ed9aee5fdec78204701d01df24871d4dd54e
duraimurugane <durai.12murugan@gmail.com>
2021-12-29 16:08:48 +0530

    counter animation  icon & scroll to top button updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare let $:any;
 
 @Component({
   selector: 'app-root',
@@ -21,6 +21,41 @@ export class AppComponent {
 //              });
 // }
 
+// ===== Scroll to Top ==== 
 
+constructor() {
+ 
+//   $(window).scroll(() => {
+//     if ($(this).scrollTop() >= 50) {        
+//         $('#return-to-top').fadeIn(200);   
+//     } else {
+//         $('#return-to-top').fadeOut(200);  
+//     }
+//   });
+//   $('#return-to-top').click(function() {      
+//     $('body,html').animate({
+//         scrollTop : 0                       
+//     }, 500);
+//   });  
+// }
+var btn = $('#button');
+
+$(function(){
+
+  //Scroll event
+  $(window).scroll(function(){
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 200) $('.go-top').fadeIn('slow');
+    if (scrolled < 200) $('.go-top').fadeOut('slow');
+  });
   
+  //Click event
+  $('.go-top').click(function () {
+    $("html, body").animate({ scrollTop: "0" },  500);
+  });
+
+});
+
+}
+
 }
