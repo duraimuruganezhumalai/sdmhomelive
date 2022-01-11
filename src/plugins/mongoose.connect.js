3abe8abee5e2559ab4d276316c784c1ce4f1e771
duraimurugane <durai.12murugan@gmail.com>
2022-01-11 18:44:36 +0530

    database contact form only pending

--- src/plugins/mongoose.connect.js
+++ src/plugins/mongoose.connect.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://localhost:27017/contactform');
 const Cat = mongoose.model('Cat', { name: String });
 
 const kitty = new Cat({ name: 'check' });
-kitty.save().then(() => console.log('fine'));
+kitty.save().then(() => console.log('database connected successfully!'));
 // getting-started.js
